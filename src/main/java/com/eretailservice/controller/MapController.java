c72c148f981725b889ab9d1fe99ff2632f63384a
yinchun <yinchun@10.255.113.50>
2017-04-21 18:10:31 -0400

    working draft 000

--- src/main/java/com/eretailservice/controller/MapController.java
+++ src/main/java/com/eretailservice/controller/MapController.java
@@ -2,16 +2,20 @@ package com.eretailservice.controller;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eretailservice.location.ServerLocation;
 import com.eretailservice.location.ServerLocationBo;
 import com.eretailservice.service.GeocodeService;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
 
 @Controller
 public class MapController {
@@ -79,9 +83,24 @@ public class MapController {
 		}
 
 		return result;
-
 	}
 
+	@RequestMapping(value = "/maps/nearby", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody
+	String getBusinessNearBy() {
+		String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyD7vnkhYbTs1oDx1mbH2QnYYBywOWdVNTM&location=-33.8670522,151.1957362&rankby=distance&types=hardware_store";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String place = (String) restTemplate.getForObject(url, String.class, 200);
+//		PlacesSearchResponse place = (PlacesSearchResponse) restTemplate.getForObject(url, PlacesSearchResponse.class, 200);
+	      
+        System.out.println("ID: " + place);
+    
+		return place;
+
+	}
+	
+	
 	/*    @Autowired
     private GeolocDAO geolocDAO;
 
